d253ef87536d15548e1baf1c4c9197ddd633545d
tcornelll <31414390+tcornelll@users.noreply.github.com>
2022-01-18 11:32:48 -0600

    Dragon Cave with Input Mismatch Exception added

---
+++ DragonCaveWithExceptions/src/main/java/DragonCaveWithExceptions.java
@@ -0,0 +1,67 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class DragonCaveWithExceptions {
+    static Random rand = new Random();
+    static Scanner sc  = new Scanner(System.in);
+    static boolean isEaten = false;
+    private static int correctChoice = rand.nextInt(2) + 1;
+
+    public static void printIntro() {
+        System.out.print("""
+                You are in a land full of dragons. In front of you,
+                you see two caves. In one cave, the dragon is friendly
+                and will share his treasure with you. The other dragon
+                is greedy and hungry and will eat you on sight
+                Which cave will you go into? (1 or 2)
+                """);
+    }
+
+    public static void handleEaten() {
+        System.out.print("You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                "Gobbles you down in one bite!");
+    }
+
+    public static void handleShared() {
+        System.out.print("You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                "Tells you that he is feeling generous...\n" +
+                "And will share his gold with you today!\n" +
+                "He gives you some of his gold\n" +
+                "And you continue on your journey\n");
+        correctChoice = rand.nextInt(2) + 1;
+    }
+
+
+
+    public static void handleChoice(int choice) {
+        System.out.println(choice);
+        double rand = Math.random();
+        if(choice == correctChoice){
+            handleShared();
+        }
+        else {
+            handleEaten();
+            isEaten = true;
+        }
+    }
+
+    public static void main(String[] args){
+        while(!isEaten){
+            printIntro();
+            try {
+                int choice = sc.nextInt();
+                handleChoice(choice);
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Caught Exception: Input Mismatch Exception (You must input an integer value)");
+                return;
+            }
+
+        }
+    }
+}
